d6cb4bff4ba52316d781bc247a0aab80a145336a
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-10-12 16:56:41 +0800

    Prepare for B2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const apiRoutes = require("./src/api-routes");
+const server = require("./server");
 
-const app = express();
-
-// Configure bodyparser to handle post requests
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
+const app = server.createExpressServer();
 
 const DB_URI = process.env.DATABASE_URI || "mongodb://localhost/contacts";
 
@@ -32,21 +22,7 @@ else console.log("Db connected successfully " + DB_CONNECTION_STRING);
 // Setup server port
 var port = process.env.PORT || 8080;
 
-// Send message for default URL
-app.get("/", (_, res) => res.send("Hello World with Express"));
-
-// Use Api routes in the App
-app.use("/api", apiRoutes);
-
-// Return 404 otherwise
-app.use("*", (_, res) => {
-  res.status(404).json({
-    status: "error",
-    message: "invalid api route!",
-  });
-});
-
 // Launch app to listen to specified port
 app.listen(port, function () {
-  console.log("Running OTOT-task B1-B2 on port " + port);
+  console.log("Running OTOT-task B1-B2 on http://localhost:" + port);
 });
---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./api-routes");
+
+exports.createExpressServer = () => {
+  const app = express();
+  // Configure bodyparser to handle post requests
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(bodyParser.json());
+
+  // Send message for default URL
+  app.get("/", (_, res) => res.send("Hello World with Express"));
+
+  // Use Api routes in the App
+  app.use("/api", apiRoutes);
+
+  // Return 404 otherwise
+  app.use("*", (_, res) => {
+    res.status(404).json({
+      status: "error",
+      message: "invalid api route!",
+    });
+  });
+  return app;
+};
